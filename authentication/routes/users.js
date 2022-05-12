d6f85c2cec5cda26f5e75f0a913014049a1509fa
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2022-05-12 19:04:12 +0530

    a few more things

---
+++ authentication/routes/users.js
@@ -0,0 +1,28 @@
+const { User, schema } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  // First Validate The Request
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // Check if this user already exisits
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).send("That user already exisits!");
+  } else {
+    // Insert the new user if they do not exist yet
+    user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await user.save();
+    res.send(user);
+  }
+});
+
+module.exports = router;
