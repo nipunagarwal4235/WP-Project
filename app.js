452a3df08def7ab89de4b2490e1e030905266580
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2022-05-18 13:36:12 +0530

    final submission

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 var express = require("express");
+const path = require("path");
+const hbs = require("hbs");
 var bodyParser = require("body-parser");
-
 const mongoose = require("mongoose");
+
+const staticPath = path.join(__dirname, "./public");
+
 mongoose.connect("mongodb://localhost:27017/pamperpaws");
 var db = mongoose.connection;
 db.on("error", console.log.bind(console, "connection error"));
 db.once("open", function (callback) {
   console.log("connection succeeded");
 });
+
 var app = express();
+
+app.use(express.static(staticPath));
+app.set("views", path.join(__dirname, "./public/views"));
+console.log(__dirname);
+// to set view engine
+app.set("view engine", "hbs");
+
+app.get("", (req, res) => {
+  res.render("index");
+});
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -37,13 +52,8 @@ app.post("/sign_up", function (req, res) {
   return res.redirect("success.html");
 });
 
-app
-  .get("/", function (req, res) {
-    res.set({
-      "Access-control-Allow-Origin": "*",
-    });
-    return res.redirect("index.html");
-  })
-  .listen(3000);
+app.set("port", process.env.PORT || 3000);
 
-console.log("server listening at port 3000");
+app.listen(app.get("port"), () => {
+  console.log("Listening on port " + app.get("port"));
+});
